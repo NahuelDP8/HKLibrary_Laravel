91384f1385583e8a95458ff6659672a99888122b
NahuelDP8 <ndp912@gmail.com>
2023-04-27 17:58:24 -0300

    Se realizo la vista de los libros
    Faltan funcionalidades de editar y crear libro.
    
    Co-Authored-By: JanoALockhart <89466894+JanoALockhart@users.noreply.github.com>

--- resources/views/adminView/librosIndex.blade.php
+++ resources/views/adminView/librosIndex.blade.php
@@ -4,32 +4,51 @@
     <title>Listado de Libros</title>
     <!-- Agregar enlaces a los estilos de Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    
 </head>
 <body>
     <div class="container">
-        <h1>Listado de Libros</h1>
+        
+    <div class="d-flex justify-content-between mb-3">
+    <h1 class="mr-auto">Listado de Libros</h1>
+    <button class="btn btn-primary ml-auto">Nuevo Libro</button>
+</div>
+        <div class="table-responsive table-lg">
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th class="table-header">Id</th>
+                        <th class="table-header">Título</th>
+                        <th class="table-header" >Descripción</th>
+                        <th class="table-header">Cantidad de Páginas</th>
+                        <th class="table-header">Precio ($)</th>
+                        <th class="table-header">Disponible</th>
+                        <th class="table-header">Imagen</th>
 
-        <div class="row">
-            <div class="col-auto">Id</div>
-            <div class="col-auto">Título</div>
-            <div class="col-auto">Descripción</div>
-            <div class="col-auto">Cantidad de Páginas</div>
-            <div class="col-auto">Precio</div>
-            <div class="col-auto">Disponible</div>
-            <div class="col-auto">Imagen</div>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($libros as $libro)
+                        <tr class="table-row">
+                            <td>{{ $libro->id }}</td>
+                            <td>{{ $libro->titulo }}</td>
+                            <td class="text-truncate" style=" max-width: 500px">{{ $libro->descripcion }}</td>
+                            <td>{{$libro->cantidadPaginas}} pags</td>
+                            <td>{{ $libro->precio }}</td>
+                            <td>
+                                @if ($libro->disponible == 1) 
+                                    "Disponible"
+                                @else 
+                                    "NO Disponible"
+                                @endif
+                            </td>
+                            <td ><img style="max-height:50px" src="{{ $libro->urlImagen }}" alt="Imagen del Libro" class="img-fluid"></td> 
+                            <td><button class="btn btn-primary">Editar</button></td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
-
-        @foreach ($libros as $libro)
-            <div class="row">
-                <div class="col-auto">{{ $libro->id }}</div>
-                <div class="col-auto">{{ $libro->titulo }}</div>
-                <div class="col-auto overflow-scroll" style="max-height: 300px; max-width: 500px">{{ $libro->descripcion }}</div>
-                <div class="col-auto">{{ $libro->cantidadPaginas }}</div>
-                <div class="col-auto">{{ $libro->precio }}</div>
-                <div class="col-auto">{{ $libro->disponible }}</div>
-                <div class="col-auto"><img src="{{ $libro->urlImagen }}" alt="Imagen del Libro"></div>
-            </div>
-        @endforeach
     </div>
 
     <!-- Agregar enlaces a los scripts de Bootstrap (jQuery y Popper.js) -->

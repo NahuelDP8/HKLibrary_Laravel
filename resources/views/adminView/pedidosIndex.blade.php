d22fd6cd519f7205105eeb7bf8a3dc04facf448a
JanoALockhart <janolockhart@gmail.com>
2023-04-30 13:08:51 -0300

    Se cambio el estilo de la tabla
    Ahora las filas son mas finas
    
    Co-Authored-By: Dueñas Pacioni Nahuel <89594152+NahuelDP8@users.noreply.github.com>

--- resources/views/adminView/pedidosIndex.blade.php
+++ resources/views/adminView/pedidosIndex.blade.php
@@ -3,31 +3,31 @@
 @section('title','Listado Pedidos')
 
 @section('content')
-<div class="container justify-content-center px-5 py-4">
+<div class="container justify-content-center py-2">
     <div class="d-flex justify-content-between">
-        <h1 class="mr-auto">Listado de Libros</h1>
+        <h1 class="mr-auto">Listado de Pedidos</h1>
     </div>
-    <div class="table-responsive table-lg">
-        <table class="table table-bordered table-striped table-hover">
+    <div class="table table-sm">
+        <table class="table table-condensed table-bordered table-striped table-hover">
             <thead>
                 <tr>
-                    <th class="table-header col-1">Nro. Pedido</th>
-                    <th class="table-header col-1 text-center">Fecha</th>
-                    <th class="table-header col-2">Cliente</th>
-                    <th class="table-header col-4">Mail</th>
-                    <th class="table-header col-1 text-center">Precio Total ($)</th>
-                    <th class="table-header col-1"></th>
+                    <th class="table-header">Nro. Pedido</th>
+                    <th class="table-header text-center">Fecha</th>
+                    <th class="table-header">Cliente</th>
+                    <th class="table-header">Mail</th>
+                    <th class="table-header text-center">Precio Total ($)</th>
+                    <th class="table-header"></th>
                 </tr>
             </thead>
             <tbody>
                 @foreach ($pedidos as $pedido)
-                    <tr class="table-row">
-                        <td>{{ $pedido->id }}</td>
-                        <td class="text-center">{{ $pedido->fecha }}</td>
-                        <td>{{ $pedido->cliente->nombre_completo}}</td>
-                        <td>{{ $pedido->cliente->mail }}</td>
-                        <td class="text-center">{{ $pedido->precio_total }}</td>
-                        <td class="text-center"><button class="btn btn-primary">Ver detalle</button></td>
+                    <tr class="table-row p-0">
+                        <td class="px-1 py-0 text-middle align-middle">{{ $pedido->id }}</td>
+                        <td class="px-1 py-0 text-center align-middle">{{ $pedido->fecha }}</td>
+                        <td class="px-1 py-0 align-middle">{{ $pedido->cliente->nombre_completo}}</td>
+                        <td class="px-1 py-0 align-middle">{{ $pedido->cliente->mail }}</td>
+                        <td class="px-1 py-0 text-center align-middle">{{ $pedido->precio_total }}</td>
+                        <td class="px-1 py-0 text-center align-middle"><button class="btn btn-primary">Ver detalle</button></td>
                     </tr>
                 @endforeach
             </tbody>

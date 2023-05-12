36a9584f2819d675c0821dadf18ef32f34526f0d
NahuelDP8 <ndp912@gmail.com>
2023-05-11 21:18:51 -0300

    Intento de arreglar las imagenes en vercel

--- resources/views/adminView/layout/layout.blade.php
+++ resources/views/adminView/layout/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/x-icon" href="{{ asset('images/iconBookShop.ico') }}" sizes="32x32">
+    <link rel="icon" type="image/x-icon" href="{{ url('images/iconBookShop.ico') }}" sizes="32x32">
     
     <!-- Agregar enlaces a los estilos de Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">

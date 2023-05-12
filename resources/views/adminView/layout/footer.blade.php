2808050c90a3367feba41d253202f57601544645
JanoALockhart <89466894+JanoALockhart@users.noreply.github.com>
2023-05-12 19:47:39 -0300

    Mejorada vista de los autores
    Se simplicaron las clases de boostrap
    
    Co-Authored-By: Dueñas Pacioni Nahuel <89594152+NahuelDP8@users.noreply.github.com>

--- resources/views/adminView/layout/footer.blade.php
+++ resources/views/adminView/layout/footer.blade.php
@@ -1,5 +1,5 @@
-<footer class="bg-light text-center sticky-bottom align-bottom">
-  <div class="text-center p-3 bg-secondary text-light align-bottom">
+<footer class="bg-light text-center sticky-bottom">
+  <div class="text-center p-3 bg-secondary text-light">
     © 2023 Página realizada con Boostrap 5
   </div>
 </footer>

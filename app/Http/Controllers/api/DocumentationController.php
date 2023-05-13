ab1cd9f8c6562e210d030ff87a76ccfa0a840fc0
JanoALockhart <89466894+JanoALockhart@users.noreply.github.com>
2023-05-13 13:29:10 -0300

    Documentacion API
    Se agrego el comentario de lo que sucede si la url para la api es incorrecta
    
    Co-Authored-By: Dueñas Pacioni Nahuel <89594152+NahuelDP8@users.noreply.github.com>

--- app/Http/Controllers/api/DocumentationController.php
+++ app/Http/Controllers/api/DocumentationController.php
@@ -8,7 +8,8 @@ use App\Http\Controllers\Controller;
  * @OA\Info(
  *      version="1.0.0",
  *      title="API Libreria",
- *      description="Esta API puede usarse para obtener información sobre los libros, autores y generos de la librería, y crear un pedido.",
+ *      description="Esta API puede usarse para obtener información sobre los libros, autores y generos de la librería, y crear un pedido.
+ *      Si la url para la API no tiene el formato adecuado o no existe, se retornará un json con un mensaje indicándoselo."
  * )
  *
  * @OA\Server(
